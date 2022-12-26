9ef346e071fd6b83c6a8bf6febdb5087a71bff03
Quoc Cuong <quoccuonga2st@gmail.com>
2022-12-26 15:03:48 +0700

    Commplete Setup Project React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Tiếng Việt</h1>
+            <p>Đây là một đoạn văn bản tiếng việt</p>
+        </div>
+    );
 }
 
 export default App;
