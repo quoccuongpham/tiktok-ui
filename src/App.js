1a34aaebb2eebe95892472e0c27e6cd8de1394d9
Quoc Cuong <quoccuonga2st@gmail.com>
2022-12-31 13:15:13 +0700

    config Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
-        <div className="App">
-            <h1>Tiếng Việt</h1>
-            <p>Đây là một đoạn văn bản tiếng việt</p>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
