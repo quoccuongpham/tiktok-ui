2c4dc0b03bf91f8e62edba219466c12e543b1bd2
Quoc Cuong <quoccuonga2st@gmail.com>
2023-01-01 11:00:47 +0700

    Dựng khung layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <h2 className={cx('wrapper')}>
+            <div className={cx('inner')}>{/* logo, search */}</div>
+        </h2>
+    );
 }
 
 export default Header;
